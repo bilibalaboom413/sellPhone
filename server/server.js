6aa08db11e9d79305b82e0765812b5c9660b389b
Hao Chen <hche8093@uni.sydney.edu.au>
2022-04-18 20:16:27 +0800

    Initialize backend

--- server/server.js
+++ server/server.js
@@ -1,2 +1,27 @@
 // Load .env
 require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const router = require("./routes/home.routes");
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/posts", router);
+
+const CONNECTION_URL = process.env.MONGO_URL;
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
