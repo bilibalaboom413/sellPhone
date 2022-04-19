1d4f33ca7f20a4dcf801ff9cf1adce9f551cdc12
Hao Chen <hche8093@uni.sydney.edu.au>
2022-04-19 21:15:33 +0800

    test database connection and functionality

--- server/controllers/home.controller.js
+++ server/controllers/home.controller.js
@@ -1,1 +1,37 @@
-const express = require("express");
+// const express = require("express");
+const Phone = require("../models/Phones");
+const User = require("../models/User");
+
+module.exports.showCheckout = function (req, res) {
+  // const id = req.query.id;
+  const id = "625d127d2140a08eb1365d2a";
+  let inventory = 0;
+  let sell = 9;
+
+  Phone.findPhoneById(id, function (err, phone) {
+    console.log(id);
+    if (err) {
+      console.log("Cannot find phone: " + id);
+    } else {
+      console.log(phone);
+      inventory = phone[0].stock;
+      // res.send(phone);
+      console.log(inventory, sell);
+      if (inventory >= sell) {
+        Phone.updateOne(
+          { _id: id },
+          { stock: inventory - sell },
+          function (err, result) {
+            if (err) {
+              console.log("Update error");
+            } else {
+              console.log(result);
+            }
+          }
+        );
+      } else {
+        throw new Error("Not enough stock of phone: " + id);
+      }
+    }
+  });
+};
--- server/routes/home.routes.js
+++ server/routes/home.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const controller = require("../controllers/home.controller");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello world");
-});
+// router.get("/", (req, res) => {
+//   res.send("Hello world");
+// });
+router.get("/checkout", controller.showCheckout);
 
 module.exports = router;
