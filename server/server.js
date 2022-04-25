d8e6d5c12b024826bc873d9549083f0c696ce070
Hilbert Kong <lkon0010@uni.sydney.edu.au>
2022-04-25 21:04:20 +0800

    Test running both Front-end and Back-end

--- server/server.js
+++ server/server.js
@@ -1,4 +1,19 @@
+const express = require('express');
+const { process_params } = require('express/lib/router');
+
 // Load .env
 require("dotenv").config();
 
-console.log(process.env.MONGO_URL);
+const app = express();
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Listening on Port ${port}`)
+});
+
+// Routes
+app.get('/helloworld', (req, res) => {
+    res.send("Hello word");
+});
+
+module.exports = app;
