a7d2b94dc59cdd77f3d9d59c57022329e6ba81f2
Hao Chen <hche8093@uni.sydney.edu.au>
2022-04-29 17:50:29 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use("/checkout", router);
 
 const CONNECTION_URL = process.env.MONGO_URL;
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 mongoose
   .connect(CONNECTION_URL)
   .then(() =>
