1f1fff3e797b72653bbf1f481e3ec0eadc6cb508
Hao Chen <hche8093@uni.sydney.edu.au>
2022-04-29 17:44:42 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Checkout from "./checkout/Checkout";
-import Home from "./Home";
-import Layout from "./Layout";
-
-// const App = () => {
-//   return (
-//     <BrowserRouter className="App">
-//       <Routes>
-//         <Route path="/" element={<Layout />}>
-//           <Route index element={<Home />} />
-//           <Route path="checkout" element={<Checkout />} />
-//         </Route>
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// };
-// import { Routes, Route } from "react-router-dom";
 
 function Test() {
   return <h1>Hello World!</h1>;
@@ -26,7 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path="/helloworld" element={<Test />} />
-      <Route paht="./checkout" element={<Checkout />} />
+      <Route path="/checkout" element={<Checkout />} />
     </Routes>
   );
 }
