646f1dc3e0f7d56f32055305c849c7d7d945f9aa
Hao Chen <hche8093@uni.sydney.edu.au>
2022-04-29 17:58:43 +0800

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,11 @@ import "./style.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
