58acc7b59b070c479715f3e92b1751ac05f23892
Li Zheng <lzhe7349@uni.sydney.edu.au>
2022-04-30 11:03:58 +0800

    Merge dev and homepage parts

---
+++ client/src/Homepage/Search.js
@@ -0,0 +1,149 @@
+import React from "react";
+import axios from "axios";
+
+class Search extends React.Component {
+  state = {
+    phones: [],
+    soldout: [],
+    brandlist: [],
+    searchInput: "",
+    BrandInput: "",
+    highValue: "",
+    setValue: 200,
+    userId: "5f5237a4c1beb1523fa3da05",
+  };
+
+  constructor() {
+    super();
+    this.getBrand();
+    this.getData();
+    this.getHighestValue();
+  }
+
+  getData = async () => {
+    axios.get("http://localhost:8000/phone").then((_d) => {
+      // console.log(_d)
+      this.setState({ phones: _d.data });
+    });
+  };
+  getBrand = async () => {
+    axios.get("http://localhost:8000/brand").then((_d) => {
+      // console.log(_d.data)
+      this.setState({ brandlist: _d.data });
+    });
+  };
+
+  getSearch = async () => {
+    const { searchInput, BrandInput, setValue } = this.state;
+    axios
+      .get("http://localhost:8000/Search", {
+        params: {
+          title: searchInput,
+          brand: BrandInput,
+          value: setValue,
+        },
+      })
+      .then((_d) => {
+        console.log(_d);
+        this.setState({ phones: _d.data });
+      });
+  };
+  getHighestValue = async () => {
+    axios.get("http://localhost:8000/highestValue").then((_d) => {
+      this.setState({ highValue: _d.data });
+      console.log(_d.data);
+    });
+  };
+  handleGetTitle = (event) => {
+    this.setState({
+      searchInput: event.target.value,
+    });
+  };
+  handleGetBrand = (event) => {
+    this.setState({
+      BrandInput: event.target.value,
+    });
+  };
+  handleGetValue = (event) => {
+    this.setState({
+      setValue: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div id="root">
+        <div id="search">
+          <input
+            type="text"
+            placeholder="Search by name"
+            value={this.state.searchInput}
+            onChange={this.handleGetTitle}
+          />
+          <select value={this.state.BrandInput} onChange={this.handleGetBrand}>
+            <option value="">Phone Brand</option>
+            {this.state.brandlist.map((brandlist) => (
+              <option>{brandlist}</option>
+            ))}
+          </select>
+          <input
+            type="range"
+            name="Value"
+            min="0"
+            max={this.state.highValue.price + 1}
+            value={this.state.setValue}
+            onChange={this.handleGetValue}
+          />
+          <a>{this.state.setValue}</a>
+          <input type="button" onClick={this.getSearch} value="search" />
+        </div>
+
+        <div>
+          <button>checkout</button>
+        </div>
+
+        <div id="searchList">
+          <table>
+            <thead>
+              <th>title</th>
+              <th>brand</th>
+              <th>image</th>
+              <th>stock</th>
+              <th>seller</th>
+              <th>price</th>
+              <th></th>
+            </thead>
+            <tbody>
+              {this.state.phones.map((phone) => (
+                <tr key={phone._id}>
+                  <td>{phone.title}</td>
+                  <td>{phone.brand}</td>
+                  <td>{phone._id}</td>
+                  <td>{phone.stock}</td>
+                  <td>{phone.seller}</td>
+                  <td>{phone.price}</td>
+                  <td>
+                    <button
+                      onClick={() =>
+                        window.open(
+                          "/addreview?bookId=" +
+                            phone._id +
+                            "&userId=" +
+                            this.state.userId
+                        )
+                      }
+                    >
+                      EditBook
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Search;
--- server/controllers/phoneController.js
+++ server/controllers/phoneController.js
@@ -1,175 +1,208 @@
-const PhoneService = require("../service/phoneservice");
-const Phone = require("../model/phone");
-module.exports = class PhoneController{
+const Phone = require("../models/Phone");
+const mongoose = require("mongoose");
+module.exports = class PhoneController {
+  static async apiGetAllPhoneService(req, res, next) {
+    try {
+      const phone = await Phone.find(
+        {},
+        {
+          _id: 1,
+          title: 1,
+          brand: 2,
+          image: 3,
+          stock: 4,
+          seller: 5,
+          price: 6,
+        }
+      );
+      if (!phone) {
+        res.status(404).json("There are no phone published yet!");
+      }
+      res.json(phone);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
 
-    static async apiGetAllPhoneService(req, res, next){
-        try {
-            const phone = await Phone.find({},{
-                _id : 1,
-                "title":1,
-                "brand":2,
-                "image":3,
-                "stock":4,
-                "seller":5,
-                "price":6
-            });
-            if(!phone){
-                res.status(404).json("There are no phone published yet!")
-            }
-            res.json(phone);
-        } catch (error) {
-            res.status(500).json({error: error})
+  static async apiGetSoldOutService(req, res, next) {
+    try {
+      const phone = await Phone.find(
+        { stock: { $gt: 0 } },
+        {
+          _id: 0,
+          title: 1,
+          price: 2,
         }
+      )
+        .sort({ stock: 1 })
+        .limit(5);
+      if (!phone) {
+        res.status(404).json("There are no phone published yet!");
+      }
+      res.json(phone);
+    } catch (error) {
+      res.status(500).json({ error: error });
     }
+  }
 
-    static async apiGetSoldOutService(req, res, next){
-        try {
-            const phone = await Phone.find({"stock" : {$gt : 0}},{
-                _id : 0,
-                "title":1,
-                "price":2,
-            }).sort({"stock":1}).limit(5);
-            if(!phone){
-                res.status(404).json("There are no phone published yet!")
-            }
-            res.json(phone);
-        } catch (error) {
-            res.status(500).json({error: error})
-        }
+  static async apiGetBestSellerService(req, res, next) {
+    try {
+      const phone = await Phone.aggregate([
+        {
+          $project: {
+            _id: 0,
+            title: 1,
+            Ave_rating: { $avg: "$reviews.rating" },
+          },
+        },
+      ])
+        .sort({ Ave_rating: -1 })
+        .limit(5);
+      if (!phone) {
+        res.status(404).json("There are no phone published yet!");
+      }
+      res.json(phone);
+    } catch (error) {
+      res.status(500).json({ error: error });
     }
+  }
 
-    static async apiGetBestSellerService(req, res, next){
-        try {
-            const phone = await Phone.aggregate([
-                {
-                    $project : {
-                        _id : 0 ,
-                        "title" : 1 ,
-                        "Ave_rating": { $avg: '$reviews.rating'}
-                    }}
-            ]).sort({"Ave_rating":-1}).limit(5);
-            if(!phone){
-                res.status(404).json("There are no phone published yet!")
-            }
-            res.json(phone);
-        } catch (error) {
-            res.status(500).json({error: error})
+  static async apiGetBrandService(req, res, next) {
+    try {
+      const phone = await Phone.find(
+        {},
+        {
+          _id: 0,
+          brand: 1,
         }
+      ).distinct("brand");
+      if (!phone) {
+        res.status(404).json("There are no phone published yet!");
+      }
+      res.json(phone);
+    } catch (error) {
+      res.status(500).json({ error: error });
     }
-    static async apiGetBrandService(req, res, next){
-        try {
-            const phone = await Phone.find({},{
-                _id : 0,
-                "brand":1,
-            }).distinct('brand');
-            if(!phone){
-                res.status(404).json("There are no phone published yet!")
-            }
-            res.json(phone);
-        } catch (error) {
-            res.status(500).json({error: error})
-        }
-    }
-    static async apiGetHighestValue(req, res, next){
-        try {
-            const phone = await Phone.find({},{
-                _id : 0,
-                "price":1,
-            }).sort({"price":-1}).limit(1);
-            if(!phone){
-                res.status(404).json("There are no phone published yet!")
-            }
-            res.send(phone[0]);
-        } catch (error) {
-            res.status(500).json({error: error})
-        }
-    }
-    static async apiAddReview(req, res, next){
-        try {
-            const id = req.query.id;
-            const reviewer = req.query.userId;
-            const rating = req.query.rating;
-            const comment = req.query.comment;
-            console.log("Controller layer")
-            const phone = await Phone.updateOne({"_id": id},
-                { "$push":
-                        {"reviews":
-                                {
-                                    "reviewer": reviewer,
-                                    "rating": rating,
-                                    "comment": comment
-                                }
-                        }
-                }
-            );
-            if(!phone){
-                res.status(404).json("There are no phone published yet!")
-            }
-            console.log("complete!")
-            console.log(phone)
-            res.send(phone);
-        } catch (error) {
-            res.status(500).json({error: error})
-        }
-    }
-    // static async apiGetUserName(req, res, next){
-    //     try {
-    //         const user = await User.find({},{
-    //             _id : 0,
-    //             "":1,
-    //         });
-    //         if(!user){
-    //             res.status(404).json("There are no user!")
-    //         }
-    //         res.json(user);
-    //     } catch (error) {
-    //         res.status(500).json({error: error})
-    //     }
-    // }
-    static async apiGetSearchService(req, res, next){
-        try {
-            const _title= new RegExp(req.query.title);//模糊查询参数
-            const _brand = new RegExp(req.query.brand);
-            const _value = (req.query.value)
-            const phone = await Phone.find({title: {$regex:_title,$options: 'i'},brand: {$regex:_brand,$options: 'i'},price:{$lte:_value}}, {
-                _id: 0,
-                "title": 1,
-                "brand": 2,
-                "image": 3,
-                "stock": 4,
-                "seller": 5,
-                "price": 6,
-            });
-            if(!phone){
-                res.status(404).json("There are no phone published yet!")
-            }
-            res.json(phone);
-        } catch (error) {
-            res.status(500).json({error: error})
-        }
-    }
-    static async apiGetPhoneInfo(req, res, next){
-        try {
-            const id = req.query.id;
-            const phone = await Phone.find({"_id": id}, {
-                _id: 0,
-                "title": 1,
-                "brand": 2,
-                "image": 3,
-                "stock": 4,
-                "seller": 5,
-                "price": 6,
-                "reviews.reviewer":7,
-                "reviews.rating":8,
-                "reviews.comment":9,
-            });
-            if(!phone){
-                res.status(404).json("There are no phone published yet!")
-            }
-            res.json(phone);
-        } catch (error) {
-            res.status(500).json({error: error})
-        }
+  }
 
+  static async apiGetHighestValue(req, res, next) {
+    try {
+      const phone = await Phone.find(
+        {},
+        {
+          _id: 0,
+          price: 1,
+        }
+      )
+        .sort({ price: -1 })
+        .limit(1);
+      if (!phone) {
+        res.status(404).json("There are no phone published yet!");
+      }
+      res.send(phone[0]);
+    } catch (error) {
+      res.status(500).json({ error: error });
     }
-}
+  }
+
+  static async apiAddReview(req, res, next) {
+    try {
+      const id = req.query.id;
+      const reviewer = req.query.userId;
+      const rating = req.query.rating;
+      const comment = req.query.comment;
+      console.log("Controller layer");
+      const phone = await Phone.updateOne(
+        { _id: id },
+        {
+          $push: {
+            reviews: {
+              reviewer: reviewer,
+              rating: rating,
+              comment: comment,
+            },
+          },
+        }
+      );
+      if (!phone) {
+        res.status(404).json("There are no phone published yet!");
+      }
+      console.log("complete!");
+      console.log(phone);
+      res.send(phone);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  // static async apiGetUserName(req, res, next){
+  //     try {
+  //         const user = await User.find({},{
+  //             _id : 0,
+  //             "":1,
+  //         });
+  //         if(!user){
+  //             res.status(404).json("There are no user!")
+  //         }
+  //         res.json(user);
+  //     } catch (error) {
+  //         res.status(500).json({error: error})
+  //     }
+  // }
+  static async apiGetSearchService(req, res, next) {
+    try {
+      const _title = new RegExp(req.query.title); //模糊查询参数
+      const _brand = new RegExp(req.query.brand);
+      const _value = req.query.value;
+      const phone = await Phone.find(
+        {
+          title: { $regex: _title, $options: "i" },
+          brand: { $regex: _brand, $options: "i" },
+          price: { $lte: _value },
+        },
+        {
+          _id: 0,
+          title: 1,
+          brand: 2,
+          image: 3,
+          stock: 4,
+          seller: 5,
+          price: 6,
+        }
+      );
+      if (!phone) {
+        res.status(404).json("There are no phone published yet!");
+      }
+      res.json(phone);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiGetPhoneInfo(req, res, next) {
+    try {
+      const id = req.query.id;
+      const phone = await Phone.find(
+        { _id: id },
+        {
+          _id: 0,
+          title: 1,
+          brand: 2,
+          image: 3,
+          stock: 4,
+          seller: 5,
+          price: 6,
+          "reviews.reviewer": 7,
+          "reviews.rating": 8,
+          "reviews.comment": 9,
+        }
+      );
+      if (!phone) {
+        res.status(404).json("There are no phone published yet!");
+      }
+      res.json(phone);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+};
