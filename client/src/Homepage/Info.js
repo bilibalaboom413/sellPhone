4a82d470160b09c16ec2c8b435fd1447759c0fca
Hao Chen <hche8093@uni.sydney.edu.au>
2022-05-05 11:35:32 +0800

    Fix bug in Info.js

--- client/src/Homepage/Info.js
+++ client/src/Homepage/Info.js
@@ -10,20 +10,23 @@ class Info extends React.Component {
     ratingInput: 5,
     userid: "",
     username: "",
-    visible:false
+    visible: false,
   };
 
   constructor(props) {
     super(props);
-    this.state.phoneid = props.phoneid
-    this.state.userid = props.userid
+    this.state.phoneid = props.phoneid;
+    this.state.userid = props.userid;
     this.getInfo();
   }
 
   // add the cart information in localStorage
-  componentDidUpdate() {
-      console.log(this.state.phones)
-    if (!isNaN(this.state.quantity) && this.state.quantity > 0) {
+  componentDidUpdate(preProps) {
+    if (
+      !isNaN(this.state.quantity) &&
+      this.state.quantity > 0 &&
+      preProps.quantity !== this.state.quantity
+    ) {
       const id = this.state.phones[0]._id;
       const title = this.state.phones[0].title;
       const price = this.state.phones[0].price;
@@ -35,7 +38,9 @@ class Info extends React.Component {
       };
       let numOfCategory = localStorage.getItem("numOfCategory");
       if (localStorage.getItem("numOfCategory")) {
-        localStorage.setItem("numOfCategory", parseInt(numOfCategory) + 1);
+        if (preProps.phoneid !== this.state.phones[0]._id) {
+          localStorage.setItem("numOfCategory", parseInt(numOfCategory) + 1);
+        }
         numOfCategory = localStorage.getItem("numOfCategory");
         localStorage.setItem(numOfCategory, JSON.stringify(phone));
       } else {
@@ -46,11 +51,11 @@ class Info extends React.Component {
   }
 
   getInfo = async () => {
-    const { bookid } = this.state;
+    const { phoneid } = this.state;
     axios
       .get("http://localhost:8000/phoneinfo", {
         params: {
-          id: bookid,
+          id: phoneid,
         },
       })
       .then((_d) => {
@@ -109,90 +114,90 @@ class Info extends React.Component {
 
   render() {
     return (
-    <div className='popup'>
-      <div className='popup_inner'>
-        <button onClick={this.props.closePopup}>close me</button>
-        <table>
-          <thead>
-            <th>title</th>
-            <th>brand</th>
-            <th>image</th>
-            <th>stock</th>
-            <th>seller</th>
-            <th>price</th>
-          </thead>
-          <tbody>
-            {this.state.phones.map((phone) => (
-              <tr key={phone._id}>
-                <td>{phone.title}</td>
-                <td>{phone.brand}</td>
-                <td>{phone._id}</td>
-                <td>{phone.stock}</td>
-                <td>{phone.seller}</td>
-                <td>{phone.price}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <table>
-          <thead>
-            <th>reviews</th>
-          </thead>
-          <tbody>
-            {this.state.phones.map((phone) => (
-              <tr key={phone._id}>
-                {phone.reviews.map((review) => (
-                  <tr>
-                    <td>{review.reviewer}</td>
+      <div className="popup">
+        <div className="popup_inner">
+          <button onClick={this.props.closePopup}>close me</button>
+          <table>
+            <thead>
+              <th>title</th>
+              <th>brand</th>
+              <th>image</th>
+              <th>stock</th>
+              <th>seller</th>
+              <th>price</th>
+            </thead>
+            <tbody>
+              {this.state.phones.map((phone) => (
+                <tr key={phone._id}>
+                  <td>{phone.title}</td>
+                  <td>{phone.brand}</td>
+                  <td>{phone._id}</td>
+                  <td>{phone.stock}</td>
+                  <td>{phone.seller}</td>
+                  <td>{phone.price}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <table>
+            <thead>
+              <th>reviews</th>
+            </thead>
+            <tbody>
+              {this.state.phones.map((phone) => (
+                <tr key={phone._id}>
+                  {phone.reviews.map((review) => (
+                    <tr>
+                      <td>{review.reviewer}</td>
 
-                    <td>{review.rating}</td>
+                      <td>{review.rating}</td>
 
-                    <td>{review.comment}</td>
-                  </tr>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <div>
-          <label>current added quantity: </label>
-          <span className="added-quantity">{this.state.quantity}</span>
-          {this.state.inputQuantity && (
+                      <td>{review.comment}</td>
+                    </tr>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div>
+            <label>current added quantity: </label>
+            <span className="added-quantity">{this.state.quantity}</span>
+            {this.state.inputQuantity && (
+              <input
+                type="text"
+                placeholder="input quantity"
+                onBlur={(e) => {
+                  this.handleInputQuantity(e);
+                }}
+              />
+            )}
+            {!this.state.inputQuantity && (
+              <input
+                onClick={() => this.setState({ inputQuantity: true })}
+                type="button"
+                value="add to cart"
+              />
+            )}
             <input
               type="text"
-              placeholder="input quantity"
-              onBlur={(e) => {
-                this.handleInputQuantity(e);
-              }}
+              placeholder="Comment"
+              value={this.state.commentInput}
+              onChange={this.handleGetComment}
             />
-          )}
-          {!this.state.inputQuantity && (
-            <input
-              onClick={() => this.setState({ inputQuantity: true })}
-              type="button"
-              value="add to cart"
-            />
-          )}
-          <input
-            type="text"
-            placeholder="Comment"
-            value={this.state.commentInput}
-            onChange={this.handleGetComment}
-          />
-          <select
-            value={this.state.ratingInput}
-            onChange={this.handleGetRating}
-          >
-            <option value="5">5</option>
-            <option value="4">4</option>
-            <option value="3">3</option>
-            <option value="2">2</option>
-            <option value="1">1</option>
-          </select>
-          <input type="button" onClick={this.addReview} value="add review" />
+            <select
+              value={this.state.ratingInput}
+              onChange={this.handleGetRating}
+            >
+              <option value="5">5</option>
+              <option value="4">4</option>
+              <option value="3">3</option>
+              <option value="2">2</option>
+              <option value="1">1</option>
+            </select>
+            <input type="button" onClick={this.addReview} value="add review" />
+          </div>
         </div>
       </div>
-    </div>
     );
   }
 }
