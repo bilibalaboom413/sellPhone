14b45aaf6adf91f122076c9b43500f1c3e73de06
Li Zheng <lzhe7349@uni.sydney.edu.au>
2022-05-09 10:14:19 +0800

    Revise the info page and add showing fullname function

--- server/controllers/phoneController.js
+++ server/controllers/phoneController.js
@@ -1,4 +1,5 @@
 const Phone = require("../models/Phone");
+const User = require("../models/User");
 const mongoose = require("mongoose");
 module.exports = class PhoneController {
   static async apiGetAllPhoneService(req, res, next) {
@@ -135,20 +136,6 @@ module.exports = class PhoneController {
     }
   }
 
-  // static async apiGetUserName(req, res, next){
-  //     try {
-  //         const user = await User.find({},{
-  //             _id : 0,
-  //             "":1,
-  //         });
-  //         if(!user){
-  //             res.status(404).json("There are no user!")
-  //         }
-  //         res.json(user);
-  //     } catch (error) {
-  //         res.status(500).json({error: error})
-  //     }
-  // }
   static async apiGetSearchService(req, res, next) {
     try {
       const _title = new RegExp(req.query.title); //模糊查询参数
@@ -182,27 +169,124 @@ module.exports = class PhoneController {
   static async apiGetPhoneInfo(req, res, next) {
     try {
       const id = req.query.id;
-      const phone = await Phone.find(
-        { _id: id },
-        {
-          _id: 1,
-          title: 1,
-          brand: 1,
-          image: 1,
-          stock: 1,
-          seller: 1,
-          price: 1,
-          "reviews.reviewer": 1,
-          "reviews.rating": 1,
-          "reviews.comment": 1,
+      const phone = await Phone.aggregate([
+            {
+              $match: {
+                "_id": new mongoose.Types.ObjectId(id)
+              }
+            }
+            , {
+              $project: {
+                "title": 1,
+                "brand": 1,
+                "image": 1,
+                "stock": 1,
+                "seller": 1,
+                "price": 1,
+
+          }
         }
+          ]
       );
       if (!phone) {
-        res.status(404).json("There are no phone published yet!");
+        res.status(404).json("There are no phone published yet!")
+      }
+      const name = await User.find({"_id": phone[0].seller}, {
+        _id: 0,
+        "firstname": 1,
+        "lastname": 2
+      });
+      phone[0].seller = name[0].firstname + " " + name[0].lastname
+      res.json(phone);
+    } catch (error) {
+      res.status(500).json({error: error})
+    }
+  }
+
+  static async apiGetReview(req, res, next) {
+    try {
+      const id = req.query.id;
+      console.log(id);
+      const phone = await Phone.aggregate([
+            {
+              $match: {
+                "_id": new mongoose.Types.ObjectId(id)
+              }
+            }
+            ,
+            {
+              $unwind: "$reviews"
+            }
+            , {
+              $project: {
+                "reviews.reviewer": 1,
+                "reviews.rating": 1,
+                "reviews.comment": 1,
+              }
+
+            },
+            {
+              $limit: 3
+            }
+          ]
+      );
+      if (!phone) {
+        res.status(404).json("There are no phone published yet!")
+      }
+      for (const x in phone) {
+        const name = await User.find({"_id": phone[x].reviews.reviewer}, {
+          _id: 0,
+          "firstname": 1,
+          "lastname": 2
+        });
+        const fullname = name[0].firstname + " " + name[0].lastname
+        phone[x].reviews.reviewer = fullname.toString()
       }
       res.json(phone);
     } catch (error) {
-      res.status(500).json({ error: error });
+      res.status(500).json({error: error})
+    }
+  }
+
+  static async apiGetAllReview(req, res, next) {
+    try {
+      const id = req.query.id;
+      console.log(id);
+      const phone = await Phone.aggregate([
+            {
+              $match: {
+                "_id": new mongoose.Types.ObjectId(id)
+              }
+            }
+            ,
+            {
+              $unwind: "$reviews"
+            }
+            , {
+              $project: {
+                "reviews.reviewer": 1,
+                "reviews.rating": 1,
+                "reviews.comment": 1,
+              }
+
+            }
+          ]
+      );
+      if (!phone) {
+        res.status(404).json("There are no phone published yet!")
+      }
+      for (const x in phone) {
+        const name = await User.find({"_id": phone[x].reviews.reviewer}, {
+          _id: 0,
+          "firstname": 1,
+          "lastname": 2
+        });
+        const fullname = name[0].firstname + " " + name[0].lastname
+        phone[x].reviews.reviewer = fullname.toString()
+      }
+      res.json(phone);
+    } catch (error) {
+      res.status(500).json({error: error})
     }
   }
 };
