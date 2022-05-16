76f0e58ee32ed4a44057ea9f2d69353d9e693176
Hao Chen <hche8093@uni.sydney.edu.au>
2022-05-16 11:43:57 +0800

    Update homepage and checkout

--- client/src/Homepage/Info.js
+++ client/src/Homepage/Info.js
@@ -96,7 +96,7 @@ class Info extends React.Component {
 
   //Just show 200 characters of each review
   getShowReview = async (data) => {
-    const { reviews, showreviews } = this.state;
+    // const { reviews, showreviews } = this.state;
     var reviewlist = [];
     for (const i in data) {
       reviewlist[i] = data[i].reviews.comment;
