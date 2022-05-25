62f55929910ec92af11d79af4d0ddb9e0e45ac1a
Hao Chen <hche8093@uni.sydney.edu.au>
2022-05-25 12:54:47 +0800

    Fix cors

--- server/server.js
+++ server/server.js
@@ -29,13 +29,14 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
  * with the querystring library (when false) or the qs library (when true).
  */
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
